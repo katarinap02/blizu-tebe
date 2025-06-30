97e4faed2e77f7d2be4de6a35a112091ef794e95
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2025-06-30 16:46:18 +0200

    initial commit

---
+++ BlizuTebe/Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BlizuTebe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlizuTebe.Database
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BlizuTebe/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlizuTebe.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
